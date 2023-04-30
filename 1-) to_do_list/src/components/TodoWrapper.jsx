6c1d8d8ab4eaae5cee1009ae95eaff2ba2739478
Deniz <deniz.cid09@gmail.com>
2023-04-30 23:25:03 +0300

    stopwatch project

--- 1-) to_do_list/src/components/TodoWrapper.jsx
+++ 1-) to_do_list/src/components/TodoWrapper.jsx
@@ -11,7 +11,7 @@ export const TodoWrapper = () => {
     const addTodo = (todo) => {
         setTodos([...todos, {id: uuidv4(), task: todo, completed: false, isEditing: false}])
     }
-    console.log(todos);
+    
 
     const toggleComplete = (id) => (
         setTodos(todos.map(todo => todo.id === id ? {...
