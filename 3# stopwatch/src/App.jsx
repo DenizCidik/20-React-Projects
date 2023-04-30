bd80d81afa57f84c0ace01f72bb6e64c92ecda1f
Deniz <deniz.cid09@gmail.com>
2023-05-01 00:58:53 +0300

    project done

---
+++ 3# stopwatch/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Stopwatch } from './components/stopwatch'
+
+function App() {
+
+  return (
+    <>
+      <Stopwatch></Stopwatch>
+    </>
+  )
+}
+
+export default App
