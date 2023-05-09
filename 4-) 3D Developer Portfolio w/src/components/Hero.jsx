fa8e09019865e0eb274ec8fc41871fefbef25275
Deniz <deniz.cid09@gmail.com>
2023-05-09 23:58:28 +0300

    scroll-down button

--- 4-) 3D Developer Portfolio w/src/components/Hero.jsx
+++ 4-) 3D Developer Portfolio w/src/components/Hero.jsx
@@ -30,7 +30,17 @@ const Hero = () => {
         <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
           <a href="#about">
             <div className='w-[35px] h-[64px] rounded-3xl border-2 border-forborder flex justify-center items-start p-2'>
-
+                <motion.dev animate={{
+                  y: [0,24,0]
+                }}
+                transition={{
+                  duration: 1.5,
+                  repeat: Infinity,
+                  repeatType: 'loop'
+                }}
+                className='w-3 h-3 rounded-full bg-forborder mb-1'
+                />
+                
             </div>
           </a>
         </div>
