3460a8b4486fd1922c5518f78661416905aec5df
Deniz <deniz.cid09@gmail.com>
2023-05-13 03:03:17 +0300

    project done

--- 4-) 3D Developer Portfolio w/src/components/About.jsx
+++ 4-) 3D Developer Portfolio w/src/components/About.jsx
@@ -8,16 +8,23 @@ import { services } from '../constants/index';
 import { fadeIn, textVariant } from '../utils/motion';
 
 const ServiceCard = ({index, title, icon}) => {
+
+  const options = {
+        max: 45,
+        scale: 1.1,
+        speed: 1000,
+        transition: true,
+        easing:"cubic-bezier(.03,.98,.52,.99)",
+        
+  }
+
+
   return (
 
-    <Tilt className='xs:w-[250px] w-full'>
+    <Tilt options={options} className='xs:w-[250px] w-full ease-in-out duration-100'>
 
-    <motion.div variants={fadeIn('right','spring', 0.5 * index, 0.75)} className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'>
-      <div options={{
-        max: 45,
-        scale: 1,
-        speed: 450
-      }} className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'>
+    <motion.div variants={fadeIn('right','spring', 0.5 * index, 0.75)} className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card '>
+      <div className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'>
 
         <img src={icon} alt={title} className='w-16 h-16 object-contain'/>
         <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
